ace5cfbee6e6789354afbd2fe3bac187d8271f69
iballa003 <iballa.montero@gmail.com>
2024-11-26 10:18:14 +0000

    Añadido un verticalScroll a la columna principal.

--- app/src/main/java/org/iesharia/minroom2/view/TareaApp.kt
+++ app/src/main/java/org/iesharia/minroom2/view/TareaApp.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
@@ -76,7 +78,8 @@ fun tareaApp(database: AppDatabase, modifier: Modifier = Modifier) {
 
 
 
-    Column(modifier = modifier.padding(start = 15.dp, end = 15.dp, top = 25.dp)) {
+    Column(modifier = modifier.padding(start = 15.dp, end = 15.dp, top = 25.dp)
+        .verticalScroll(rememberScrollState())) {
         Row {
             Button(onClick = {tareaView = true},
                 enabled = !tareaView,
